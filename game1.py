959beec57609ebf4b5c29c98b52155fc57b19186
Bobby Johnson <woodshop099@gmail.com>
2023-01-05 20:55:39 -0500

    Third Question

--- game1.py
+++ game1.py
@@ -4,3 +4,10 @@ if problem1 == '2':
     print('That is correct')
 else:
     print('That is incorrect')
+
+problem1 = input('What is 2+2? ' )
+
+if problem1 == '4':
+    print('That is correct')
+else:
+    print('That is incorrect')
